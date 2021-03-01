4c4f4c104995934b982ac089352427becf06b271
ariful4966 <ariful4966@gmail.com>
2021-03-02 00:45:58 +0600

    Variable dicler, keyword, and Type learning

--- Basic-Js/hello.js
+++ Basic-Js/hello.js
@@ -1,3 +1,12 @@
-console.log(21);
-console.log('Good Morning');
+var bananaPrice = 12;
 
+console.log(bananaPrice);
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = 'Kolimuddin Zuckerburg';
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Programming hero Webdevelopment Course
 
 ## Basic Javascipt
+    01. Variable Dicler
+    02. Javascrit keyword
+    03. Variable Type Number
+    03. Variable Type String
+    03. Variable Type Boolean
