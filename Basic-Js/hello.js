e86a5f34ad42e05ee44518286a82086b1307fdf6
ariful4082 <ariful4082@gmail.com>
2021-03-21 01:13:58 +0600

    meth method like as absolute Number, round, ceiling, floor

--- Basic-Js/hello.js
+++ Basic-Js/hello.js
@@ -1,17 +1,7 @@
-var price1 = 'Justin';
-var name = 'Bieber';
-var result = price1 +' '+ name
-var price2 = 35;
-// price2 += 1
-// price2 ++
-price2--
-
-// var total = price2 + price1;
-// var total = price2 - price1;
-// var total = price2 * price1;
-// var total = price2 / price1;
-var total = price2 % price1;
-
-console.log(total);
-
+var number = Math.random()*100;
+// var absoluteNumber = Math.abs(number)
+// var reuslt = Math.round(number)
+// var result = Math.ceil(number)
+// var result = Math.floor(number)
+var result = Math.round(number)
 console.log(result);
