ac3c87ede7e13a2239e08960219931594ed17cf1
ariful4082 <ariful4082@gmail.com>
2021-03-21 23:30:27 +0600

    basice functions

---
+++ function.js
@@ -0,0 +1,13 @@
+function sayLovYou() { 
+    console.log('Jaan o baby');
+    console.log("Billy Baby....... Koi Tumi");
+ }
+
+ sayLovYou()
+
+ var date = 14;
+ var place = 'Restaurent';
+
+ sayLovYou();
+ var hangoutPlace = 'TSC';
+ sayLovYou()
