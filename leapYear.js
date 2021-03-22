0fe0438fe814059aa8f15e8a1a138ea84ac72623
ariful4082 <ariful4082@gmail.com>
2021-03-22 23:08:35 +0600

    how to find leapYear

--- leapYear.js
+++ leapYear.js
@@ -1,9 +1,14 @@
-const year = 3997;
-const remainder = year % 4;
+function isLeapYear(year) {
+    // const remainder = year % 4;
 
-if(remainder == 0){
-    console.log('Your Year is a Leap Year');
-}else{
-    console.log('Your year is not a Leap Year');
+    // if (remainder == 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0
 }
-// console.log(remainder == 0);
+
+const check2000 = isLeapYear(2000)
+console.log(check2000);
