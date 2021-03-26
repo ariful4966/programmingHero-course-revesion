b80b5a7172058b8e9c80149110716ec0f976e9ec
ariful4082 <ariful4082@gmail.com>
2021-03-26 12:07:42 +0600

    name compare

--- assignment.js
+++ assignment.js
@@ -28,14 +28,47 @@ console.log(wood);
 
 // 03 function
 
-function brickCalculator() {
-    
+function brickCalculator(cieling) {
+    var oneFeet = 1000;
+    var oneTotenCile = 15;
+    var elevenToTowenty = 12;
+    var towentyOneup = 10;
+
+    if (cieling <= 10) {
+        var fristLavel = oneFeet * oneTotenCile * cieling;
+        return fristLavel
+    }
+    else if (cieling > 10 && cieling <= 20) {
+        var secondLavel = oneFeet * 10 * oneTotenCile;
+        secondLavel += oneFeet * (cieling - 10) * elevenToTowenty;
+        // secondLavel *= eleventToTowenty
+        return secondLavel
+    }
+    else {
+        var thirdLavel = oneFeet  * oneTotenCile * 10; //150000
+            thirdLavel  += oneFeet * 10 * elevenToTowenty; //120000
+         thirdLavel += oneFeet * (cieling - 20) * towentyOneup // 100000
+
+        return thirdLavel
+    }
+
 }
 
+var totalBricks = brickCalculator(25)
+console.log(totalBricks);
 // 04 function
 
 function tinyFriend(name) {
+    var singleName = []
+    for(var i = 0; i< name.length; i++){
+        var element = name[i];
+        var oneName = element.length
+        singleName.push(element)
+    }
 
 
-    // return smallName
+    return singleName
 }
+var name =['Azizul' , 'Asia']
+var result = tinyFriend(name);
+console.log(result);
