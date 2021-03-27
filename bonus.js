8d3582fbd8b65a3595ce98a1551ce5a8bdfc0f69
ariful4082 <ariful4082@gmail.com>
2021-03-28 01:42:21 +0600

    variable, array, forloop

--- bonus.js
+++ bonus.js
@@ -1,5 +1,6 @@
-let number = 59;
-number = 79;
-number = 119;
-const name = 'Asad';
-console.log(number);
+var marks = [89, 97, 89, 68, 94, 86, 79];
+
+for (var i = 0; i < marks.length; i++) {
+    var element = marks[i]
+    console.log(element);
+}
