eb88ef56c0af8ce38ac1f30d0c77917ea88492d7
ariful4082 <ariful4082@gmail.com>
2021-03-28 19:02:10 +0600

    thinking functional consept vs for loop

--- bonus.js
+++ bonus.js
@@ -4,3 +4,15 @@ for (var i = 0; i < marks.length; i++) {
     var element = marks[i]
     console.log(element);
 }
+function add(number1, number2) {
+    var total = number1 + number2;
+    return total
+}
+var result1 = add(65, 89);
+console.log(result1);
+var ada = 'Adam Sandler';
+var kodom = 'Kodom Sandler';;
+console.log('not adding this time', ada);
+console.log('not eating right time', kodom);
+var result2 = add(144, 568);
+console.log(result2);
