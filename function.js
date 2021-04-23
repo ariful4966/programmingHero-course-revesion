034509d84ed126ae46670c75b5f3f5ed16b6e8ed
ariful4082 <ariful4082@gmail.com>
2021-04-23 11:57:10 +0600

    function return statement 19.2

--- function.js
+++ function.js
@@ -1,20 +1,34 @@
 // console.log(12);
 
+function evenify(num) {
+    let result;
+    if (num % 2 == 0) {
+        result = num
+    } else {
+        result = num * 2
+    }
+    return result
 
+}
+var result = evenify(13);
+var square = result * result;
+console.log('square', square);
+console.log('result', result);
 function evenify_all(nums) {
+
+    var even_array = []
     for (let i = 0; i < nums.length; i++) {
         const num = nums[i];
-        if (num % 2 == 0) {
-            console.log(num, ':is event number');
-        } else {
-            console.log(num * 2, ':is odd number');
-        }
+        var reusult = evenify(num)
+        even_array.push(result)
     }
-    
+    return even_array
+
 }
-const nums = [5, 12, 89, 45, 18, 8];
-evenify_all(nums)
+var nums = [5, 12, 89, 45, 18, 8];
+var nums_array = evenify_all(nums)
+console.log(nums_array);
 
 
-const friends_age = [13, 17, 19, 20, 18];
-evenify_all(friends_age)
+friends_age = [13, 17, 19, 20, 18];
+// evenify_all(friends_age)
