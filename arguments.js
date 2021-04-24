dbda7e2cb7d83201e3b3f3fa4d2037de38138e12
ariful4082 <ariful4082@gmail.com>
2021-04-24 16:28:00 +0600

    code organization 19.5

--- arguments.js
+++ arguments.js
@@ -1,10 +1,19 @@
 function addNumber(num1, num2) {
     var sum = 0;
+    
     for (let i = 0; i < arguments.length; i++) {
         const num = arguments[i];
         sum = sum + num;
     }
-    arguments.push(15)
+
+    function logInfo(message) {
+        console.log(message);
+    }
+    logInfo('Good Morning');
+
+    var double = sum * 2;
+
+    
     return sum
 }
 
