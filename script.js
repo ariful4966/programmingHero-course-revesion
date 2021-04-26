37596ade618ac84412593404b2b063f571e3eafb
ariful4082 <ariful4082@gmail.com>
2021-04-26 15:45:06 +0600

    change increse and decrese phone and casin price

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
+//phone Pices ++
 const phonePlus = document.querySelector('#phonePlus')
 phonePlus.addEventListener('click', () => {
     const phonePice = piceUpdate('#phonePice');
 
-    const totalPrice = priceUpdate('#mainPrice', phonePice)
+    const totalPrice = priceUpdate('#singlePhone', phonePice)
     document.querySelector('#phonePrice').innerText = totalPrice;
 })
 
+//Phone Pices --
 const phoneMinus = document.querySelector('#phoneMinus');
 phoneMinus.addEventListener('click', () => {
     const phonePice = piceUpdateMinus('#phonePice')
-    console.log(phonePice);
 
-    const totalPrice = priceUpdate('#mainPrice', phonePice)
+    const totalPrice = priceUpdate('#singlePhone', phonePice)
     document.querySelector('#phonePrice').innerText = totalPrice;
 
+})
+
+//Casin Pices ++
+const casePlus = document.querySelector('#casePlus');
+casePlus.addEventListener('click', ()=>{
+    const casePice = piceUpdate('#casePice');
+    
+    const totalPrice = priceUpdate('#singleCase', casePice)
+    document.querySelector('#casPrice').innerText = totalPrice;
+})
+
+//Casin Minus --
+const caseMinus = document.querySelector('#caseMinus');
+caseMinus.addEventListener('click', ()=>{
+    const casePice = piceUpdateMinus('#casePice');
+    
+    const totalPrice = priceUpdate('#singleCase', casePice)
+    document.querySelector('#casPrice').innerText = totalPrice;
 })
 
 function piceUpdate(id) {
@@ -23,9 +42,17 @@ function piceUpdate(id) {
 
 }
 function piceUpdateMinus(id) {
-    const phonePice = document.querySelector(id).value--;
+    const phonePice = document.querySelector(id).value;
+    if(phonePice > 1){
+         document.querySelector(id).value--;
         const piceNumber = parseFloat(phonePice);
         return piceNumber - 1
+    }else{
+        document.querySelector(id).value;
+        const piceNumber = parseFloat(phonePice);
+        return piceNumber 
+    }
+        
     
 
 }
