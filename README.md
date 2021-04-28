23eda1f5d92158024eb477ec782c184e765e50ac
ariful4082 <ariful4082@gmail.com>
2021-04-28 23:12:00 +0600

    global variable, IIFE and function expresions

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Programming hero Webdevelopment Course
 
- ## Simple Shopping card Functionalitis ##
- ***Practice Javascript Project***
-    solved by couse producer
+ 
---
+++ functionGeneral.js
@@ -0,0 +1,16 @@
+function addTodo(task){
+    const newTaskElement = document.createElement('li')
+}
+
+var myTodo = function(task){
+    
+}
+
+var name = 'Rashed';
+//IIFE
+(function addUser(){
+    romanticName = 'Jantoos'
+    console.log(romanticName);
+})();
+
+//
