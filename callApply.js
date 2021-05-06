0d1a94c4319f54ee6f339be35be1b47b3d1c1785
ariful4082 <ariful4082@gmail.com>
2021-05-06 23:35:34 +0600

    object method of javascript is bind() 26.4

--- callApply.js
+++ callApply.js
@@ -6,11 +6,30 @@ const normalPerson = {
         console.log(this.fristName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this);
         this.salary= this.salary - amount;
         return this.salary;
     }
 }
 
-normalPerson.chargeBill(150);
-normalPerson.chargeBill(3000);
-console.log(normalPerson.salary)
+const heroPerson = {
+    firstName: 'Hero',
+    lastName: 'Balam',
+    salary: 25000
+}
+const friendlyPerson = {
+    firstName: 'Hero',
+    lastName: 'Golam',
+    salary: 25000
+}
+// normalPerson.chargeBill();
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargeBill(2000)
+heroChargeBill(3000)
+console.log(heroPerson.salary);
+console.log(normalPerson.salary);
+
+const friendlyPersonChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+friendlyPersonChargeBill(3000)
+friendlyPersonChargeBill(2000);
+console.log(friendlyPerson.salary);
