bb30bb85e7cd5136e9cc59bbdb54baf80a3540ce
ariful4082 <ariful4082@gmail.com>
2021-05-17 05:53:16 +0600

    use dynami diffrent data in same component using props 32.7

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Programming Hero Course Revision
 
 ## React Core Concept
+- [x] Similer in Patern 
+- [x] Different in Data
--- src/App.js
+++ src/App.js
@@ -2,30 +2,25 @@
 import './App.css';
 
 function App() {
-const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman']
+  const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman']
   return (
     <div className="App">
       <header className="App-header">
 
         <p>I am React Person</p>
-        <Person name={nayoks[0]} nayika="Mousumi" />
-        <Person name="Josim" nayika="Shabana" />
-        <Person name="BappaRaz" nayika="Cheka"/>
-        <Person name="Elias Kanson" nayika="Bobita"/>
+        <Person name="Munna" job="football"></Person>
+        <Person name="Masum" job="Dorshok"></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
 function Person(props) {
-  const personStyle =
-  {
-    border: '2px solid red', margin: '10px'
-  }
-  console.log(props);
+
   return (
-    <div style={personStyle}>
-      <h1>Name: {props.name}</h1>
-      <h3>Hero of {props.nayika}</h3>
+    <div style={{border: '2px solid blue', width:'400px'}}>
+      <h3>My Name: {props.name}</h3>
+      <p>My Profession: {props.job}</p>
     </div>
   )
 }
