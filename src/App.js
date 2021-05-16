b4522637caa54b8d3e8b6298511f845c3ebe55c3
ariful4082 <ariful4082@gmail.com>
2021-05-16 23:11:10 +0600

    discus jsx in react 32.4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Dr. Mahfuz",
+    job: "Singer"
+  }
+  var person2 = {
+    name: 'Eva Rahman',
+    job: 'Kokil Konthi'
+  }
+  var style ={
+    color:'red',
+    background: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +21,9 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <h1 style={style}> {person.name + ' ' + person.job}</h1>
+        <h3 style={{color: 'saddlebrown', background: 'gray'}}> {person2.name + ' ' + person2.job}</h3>
+        <p>My frist react Pragraph</p>
 
       </header>
     </div>
