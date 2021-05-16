a1530b50eb55bfd1e26729714463ba3ef2941638
ariful4082 <ariful4082@gmail.com>
2021-05-17 05:43:02 +0600

    practice defferent data as a same component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-
+const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman']
   return (
     <div className="App">
       <header className="App-header">
 
         <p>I am React Person</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name={nayoks[0]} nayika="Mousumi" />
+        <Person name="Josim" nayika="Shabana" />
+        <Person name="BappaRaz" nayika="Cheka"/>
+        <Person name="Elias Kanson" nayika="Bobita"/>
       </header>
     </div>
   );
 }
-function Person() {
+function Person(props) {
   const personStyle =
   {
     border: '2px solid red', margin: '10px'
   }
+  console.log(props);
   return (
     <div style={personStyle}>
-      <h1>Name: Sakib Al Hassan</h1>
-      <h3>Hero of the Year</h3>
+      <h1>Name: {props.name}</h1>
+      <h3>Hero of {props.nayika}</h3>
     </div>
   )
 }
