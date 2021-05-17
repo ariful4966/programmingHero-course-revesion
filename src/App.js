f21e69afaec0bd933855b870666229353536ed5d
ariful4082 <ariful4082@gmail.com>
2021-05-18 00:25:03 +0600

    how to write dand destructure usestate 33.7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nayoks = ['Jasim', 'DeepJol', 'BappaRaz']
   return (
     <div className="App">
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
+      <MovieCounter />
+      <Nayok name={nayoks[1]} age="56"></Nayok>
+      <Nayok name="Sakib Khan"></Nayok>
+      <Nayok name={nayoks[0]}></Nayok>
+      <Nayok name={nayoks[2]}></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
@@ -15,15 +18,25 @@ function App() {
   );
 }
 
-function Nayok() {
-  const nayokStyle={
+function MovieCounter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <button>Add Movie</button>
+      <h5>Number of movies: {count}</h5>
+    </div>
+  )
+}
+
+function Nayok(props) {
+  const nayokStyle = {
     border: '2px solid purple',
     margin: '20px'
   }
   return (
     <div style={nayokStyle}>
-      <h1>Ami Khol Nayok</h1>
-      <h3>I have done five movies</h3>
+      <h1>Ami Nayok {props.name}</h1>
+      <h3>I have done five movies in {props.age || 30} year</h3>
     </div>
   )
 }
