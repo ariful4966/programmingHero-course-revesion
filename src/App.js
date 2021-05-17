c3baad39771ef420a52ffe0de997c494ce7e8bfa
ariful4082 <ariful4082@gmail.com>
2021-05-17 06:09:18 +0600

    pass object to components and access object 32.8

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import './App.css';
 
 function App() {
-  const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman']
+  // const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman']
+  const products = [
+    {name: 'Photoshop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'PDF Reader', price: '$6.99'},
+
+]
   return (
     <div className="App">
       <header className="App-header">
 
         <p>I am React Person</p>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
         <Person name="Munna" job="football"></Person>
         <Person name="Masum" job="Dorshok"></Person>
         <Person></Person>
@@ -15,10 +23,30 @@ function App() {
     </div>
   );
 }
+
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: '#696969',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  const {name, price}=props.product;
+  console.log(name, price);
+  return (
+    <div style={productStyle}>
+      <h2>{name}</h2>
+      <h1>{price}</h1>
+      <button>Buy Now</button>
+    </div>
+  )
+}
 function Person(props) {
 
   return (
-    <div style={{border: '2px solid blue', width:'400px'}}>
+    <div style={{ border: '2px solid blue', width: '400px' }}>
       <h3>My Name: {props.name}</h3>
       <p>My Profession: {props.job}</p>
     </div>
