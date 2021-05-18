ce0d50c9146f0b9a621c73a16e3855fd6c7c250f
ariful4082 <ariful4082@gmail.com>
2021-05-18 22:42:51 +0600

    Ema-John SPA Insall and change favicon 34.2

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 # Programming Hero Course Revision
 
-## Basic React Concepts Recap
-
-- [x] useState
-- [x] useEffect
-
-### How React work
-- [x] Virtual DOM
-- [x] Rerender
-- [x] Fiber Engin
-- [x] dif Algorithom
-- [x] Reconcliation
+## Ema-John
+**Simple SPA**
--- src/App.js
+++ src/App.js
@@ -1,62 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
-  // const nayoks = [{name:'Jasim', age: 56}, {name:'DeepJol', age: 61}, {name: 'BappaRaz', age: 30}, {name:'Omar Sani', age: 35}, {name:'Alomgir', age: 67}]
-  const [nayoks, setNayoks] = useState([])
-  useEffect(() => {
-    fetch('https://randomuser.me/api/?results=15')
-      .then(res => res.json())
-      .then(data => {
-        setNayoks(data.results)
-      })
-  }, [])
   return (
     <div className="App">
-      <MovieCounter />
-      {
-        nayoks.map((nk, id) => <Nayok key={id} name={nk.name} phone={nk.cell}></Nayok>)
-      }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
-}
-
-function MovieCounter() {
-  let [count, setCount] = useState(0)
-
-  const handleClick = () => setCount(count + 1)
-  return (
-    <div>
-      <button onClick={handleClick}>Add Movie</button>
-      <h3>Number of movies: {count}</h3>
-      <MovieDisplay movies={count}></MovieDisplay>
-      <MovieDisplay movies={count + 10}></MovieDisplay>
-      <MovieDisplay movies={count + 5}></MovieDisplay>
-      <MovieDisplay movies={count}></MovieDisplay>
-    </div>
-  )
-}
-
-function MovieDisplay(props) {
-  return <h4>Movies I have acted: {props.movies}</h4>
-}
-
-function Nayok(props) {
-  const { title, first, last } = props.name;
-  const nayokStyle = {
-    border: '2px solid purple',
-    margin: '20px'
-  }
-  return (
-    <div style={nayokStyle}>
-      <h1> {title} {first} {last}</h1>
-      <h3>Phone Number {props.phone}</h3>
-    </div>
-  )
 }
 
 export default App;
