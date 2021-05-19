7de6b1ac5e237a38a1245ee50746262219248bde
ariful4082 <ariful4082@gmail.com>
2021-05-20 00:30:20 +0600

    cart state setup and update cart on add button click 34.12

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,35 +4,38 @@ import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    
-    const [products, setProducts] = useState([])
-    useEffect(()=>{
+
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+    useEffect(() => {
         const first10 = fakeData.slice(0, 10)
         setProducts(first10)
-    },[])
+    }, [])
 
-    const handleAddProduct = (product)=>{
-        console.log('Product Added', product);
+    const handleAddProduct = (product) => {
+
+        const newCart = [...cart, product];
+        setCart(newCart)
+
     }
-    
-    
-    console.log(products);
+
+
     return (
         <div className="shop-container">
             <div className="product-container">
-            <ul>
                 {
-                    products.map((product, id)=><Product
-                     key={id} 
-                     product={product} 
-                     handleAddProduct={handleAddProduct}
-                     ></Product>)
+                    products.map((product, id) => <Product
+                        key={id}
+                        product={product}
+                        handleAddProduct={handleAddProduct}
+                    ></Product>)
                 }
-            </ul>
             </div>
-           <div className="card-container">
-               <h3>This cart container</h3>
-           </div>
+            <div className="card-container">
+                <h3>This cart container</h3>
+                <h5>Order Summary: {cart.length}</h5>
+                
+            </div>
         </div>
     )
 }
