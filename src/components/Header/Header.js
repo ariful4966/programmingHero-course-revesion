01e0e97ac5889828fe2c5c1b6e1914c9e020eac5
ariful4082 <ariful4082@gmail.com>
2021-05-19 06:08:11 +0600

    create a component, folder structure, import website logo 34.4

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import logo from '../../images/logo.png';
+
+import './Header.css'
+const Header = ()=>{
+    return(
+        <div className="header">
+            <img src={logo} alt="LOGO" />
+        </div>
+    )
+}
+
+export default Header;
