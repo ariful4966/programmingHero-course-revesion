6e3bf47f770aa4b632a998a6761168234026c03b
ariful4082 <ariful4082@gmail.com>
2021-05-27 08:26:04 +0600

    react fragment empty element 36.7

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { Button } from 'react-bootstrap';
 import News from './components/News/News';
 import Header from './components/Header/Header';
 import TopHeadline from './components/TopHeadline/TopHeadline';
+import Fragment from './components/Fragment/Fragment'
 
 function App() {
   return (
     <div className="">
       <h2>React Bootstrap</h2>
       <Header></Header>
+      <Fragment/>
       <TopHeadline></TopHeadline>
-      <News></News>
       
     </div>
   );
---
+++ src/components/Fragment/Fragment.jsx
@@ -0,0 +1,11 @@
+
+const Fragment = () => {
+    return (
+        <>
+            <h1>My Header</h1>
+            <button>Click Me</button>
+        </>
+    );
+};
+
+export default Fragment;
