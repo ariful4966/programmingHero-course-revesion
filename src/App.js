09344ce0bdff99a664ad3583051e219a5d21e053
ariful4082 <ariful4082@gmail.com>
2021-05-31 08:36:24 +0600

    nested router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
+import React from 'react';
+import Nested from './components/Nested-Route'
 
-import { useState, useEffect } from 'react';
-import './App.css';
-import Header from './components/Header/Header'
-import Shop from './components/Shop/Shop'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import fakeData from './fakeData';
-import Review from './components/Review/Review';
-import ManageInventory from './components/ManageInventory/ManageInventory';
-import NotFound from './components/NotFound/NotFound';
-import ProductDetail from './components/ProductDetail/ProductDetail';
-
-function App() {
-  const [products, setProducts] = useState([]);
-
-  const [cart, setCart] = useState([])
-  useEffect(() => {
-    const first10 = fakeData.slice(0, 10)
-    setProducts(first10)
-  }, [])
+const App = () => {
   return (
-    <Router >
-      <Header cart={cart} products={products} />
-      <Switch>
-        <Route path="/shop">
-          <Shop cart={cart} products={products} setCart={setCart} />
-        </Route>
-        <Route path="/review">
-          <Review products={fakeData} />
-        </Route>
-        <Route path="/manage">
-          <ManageInventory />
-        </Route>
-        <Route path="/product/:pdKey">
-          <ProductDetail cart={cart} products={products} setCart={setCart} />
-        </Route>
-        <Route exact path="/">
-          <Shop cart={cart} products={products} setCart={setCart} />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Nested/>
+    </div>
   );
-}
+};
 
 export default App;
