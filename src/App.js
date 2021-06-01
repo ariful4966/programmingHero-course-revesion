212d6f5f505c587a027a64f3b3f98074a8b2cbf7
ariful4082 <ariful4082@gmail.com>
2021-06-01 08:11:48 +0600

    create route parameter and read from route parameter 38_5.3

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { useEffect, useState } from 'react';
 
 import Home from './components/Home/Home';
 import NoMatch from './components/NoMatch/NoMatch';
+import FriendDetail from './components/FriendDetail/FriendDetail';
 
 function App() {
 
@@ -19,6 +20,9 @@ function App() {
       <Switch>
         <Route path="/home">
           <Home/>
+        </Route>
+        <Route path="/friend/:id">
+          <FriendDetail/>
         </Route>
         <Route exact path="/">
           <Home />
--- src/components/Friend/index.jsx
+++ src/components/Friend/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Friend = (props) => {
-    const { name, email } = props.friend;
+    const { name, email , id} = props.friend;
     const friendsStyle = {
         border: '1px solid gray',
         margin: '40px',
@@ -12,6 +13,7 @@ const Friend = (props) => {
         <div style={friendsStyle}>
             <h2>Name: {name}</h2>
             <p>Email: {email}</p>
+            <p>id: <Link to={`/friend/${id}`}>Show Detail of Id: {id}</Link></p>
         </div>
     );
 };
