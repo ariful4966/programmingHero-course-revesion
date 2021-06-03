3deca09ae0bd6529395045c00f3acda5b8b1d04a
ariful4082 <ariful4082@gmail.com>
2021-06-03 08:22:30 +0600

    share state with siblling component 39.2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Friend from '../Friend';
+import React from 'react';
 
-const Home = () => {
-    const [friends, setFriends]=useState([])
-
-  useEffect(()=>{
-    axios('https://jsonplaceholder.typicode.com/users')
-    .then(data=>setFriends(data.data))
-  },[])
-  return (
-    <div>
-      <h2>All Friends: {friends.length}</h2>
-      {
-        friends.map((friend, idex)=><Friend key={idex} friend={friend}></Friend>)
-      }
-    </div>
-  );
+const Home = ({count}) => {
+    return (
+        <div>
+            <h1>This is Home :{count}</h1>
+        </div>
+    );
 };
 
 export default Home;
