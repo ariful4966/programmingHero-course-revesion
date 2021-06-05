aabde4bb570970cc4fa6a1002fdc5d4314de2a6b
ariful4082 <ariful4082@gmail.com>
2021-06-05 22:07:44 +0600

    handle multiple data loading style and nested data access 40_5.2

--- src/App.js
+++ src/App.js
@@ -2,29 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
   const [isLike, setIsLike] = useState(' ');
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser]=useState({});
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+    //single User
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+      .then(res => res.json())
+      .then(data => setSingleUser(data))
+
+    //Random User
+    fetch('https://randomuser.me/api')
+    .then(res=>res.json())
+    .then(data=>setRandomUser(data.results[0]))
+  }, [])
   return (
     <div className="App">
-      <AccessAlarmIcon  />
-      <ThumbUpAltIcon onClick={()=>setIsLike(isLike ? '' : 'primary')} color={isLike} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AccessAlarmIcon />
+      <ThumbUpAltIcon onClick={() => setIsLike(isLike ? '' : 'primary')} color={isLike} />
+      <h1>Name: {singleUser.name}</h1>
+      <h2>Random Gender: {randomUser.name?.first}- {randomUser.email}</h2>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
     </div>
   );
 }
