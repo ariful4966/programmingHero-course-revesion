c34416f0aa1d92cf044e990841c4387bd729e912
ariful4082 <ariful4082@gmail.com>
2021-06-06 08:18:47 +0600

    install firebase, initialize firebase app 41.5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+firebase.initializeApp(firebaseConfig)
 
 function App() {
+
+  const provider = new firebase.auth.GoogleAuthProvider();
   return (
     <div className="App">
-     
+     <button>🇬 Sign in</button>
     </div>
   );
 }
