9e0f2304adb5bac900b708dc67258d17ff2c4491
ariful4082 <ariful4082@gmail.com>
2021-06-06 07:53:05 +0600

    create simple authentication project firebase config 41.4

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import { useEffect, useState } from 'react';
-
 
 function App() {
-  const [isLike, setIsLike] = useState(' ');
-  const [users, setUsers] = useState([]);
-  const [singleUser, setSingleUser] = useState({});
-  const [randomUser, setRandomUser]=useState({});
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => setUsers(data))
-
-    //single User
-    fetch('https://jsonplaceholder.typicode.com/users/1')
-      .then(res => res.json())
-      .then(data => setSingleUser(data))
-
-    //Random User
-    fetch('https://randomuser.me/api')
-    .then(res=>res.json())
-    .then(data=>setRandomUser(data.results[0]))
-  }, [])
   return (
     <div className="App">
-      <AccessAlarmIcon />
-      <ThumbUpAltIcon onClick={() => setIsLike(isLike ? '' : 'primary')} color={isLike} />
-      <h1>Name: {singleUser.name}</h1>
-      <h2>Random Gender: {randomUser.name?.first}- {randomUser.email}</h2>
-      {
-        users.map(user => <li>{user.name}</li>)
-      }
+     
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDHFNgoF7werBQ2pHozJZMNxAYm_L9aHoY",
+    authDomain: "ema-john-simple-2bc72.firebaseapp.com",
+    projectId: "ema-john-simple-2bc72",
+    storageBucket: "ema-john-simple-2bc72.appspot.com",
+    messagingSenderId: "301787552275",
+    appId: "1:301787552275:web:3826d2e21a8f859ad9fde6"
+};
+export default firebaseConfig;
