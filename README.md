d6d764a97f2f7c922dd52d065ffe1668024ac65b
ariful4082 <ariful4082@gmail.com>
2021-07-02 18:39:30 +0600

    basic app setup and fakedata ready

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 # Programming Hero Course Revision
 
-## Ema-John
-**Simple SPA**
-
-## SPA Router
-
-- [x] Router Path
-- [x] Exact Path
-- [x] No Match Path
-
-## Button Show
-- [x] Condition Show
+## Travel Guru
+***Single Page Application***
--- src/App.js
+++ src/App.js
@@ -1,69 +1,24 @@
-
-import { useState, useEffect, createContext } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header'
-import Shop from './components/Shop/Shop'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import fakeData from './fakeData';
-import Review from './components/Review/Review';
-import ManageInventory from './components/ManageInventory/ManageInventory';
-import NotFound from './components/NotFound/NotFound';
-import ProductDetail from './components/ProductDetail/ProductDetail';
-import Login from './components/Login/Login';
-import Shipment from './components/Shipment/Shipment';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-
-export const UserContext = createContext()
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const[loggedInUser, setLoggedInUser]=useState({})
-
-  const [cart, setCart] = useState([])
-  useEffect(() => {
-    const first10 = fakeData.slice(0, 10)
-    setProducts(first10)
-  }, [])
   return (
-    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-    <Router >
-      <Header cart={cart} products={products} />
-      <Switch>
-        <Route path="/shop">
-          <Shop cart={cart} products={products} setCart={setCart} />
-        </Route>
-        <Route path="/review">
-          <Review products={fakeData} />
-        </Route>
-        <PrivateRoute path="/orders">
-          <ManageInventory />
-        </PrivateRoute>
-        <Route path="/login">
-          <Login></Login>
-        </Route>
-        <PrivateRoute path="/shipment">
-          <Shipment/>
-        </PrivateRoute>
-        <Route path="/orders">
-          <ManageInventory />
-        </Route>
-        <Route path="/product/:pdKey">
-          <ProductDetail cart={cart} products={products} setCart={setCart} />
-        </Route>
-        <Route exact path="/">
-          <Shop cart={cart} products={products} setCart={setCart} />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
-    </UserContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
