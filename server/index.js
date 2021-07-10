8e431858f453e73d9868188898c56b39b7ad1945
ariful4082 <ariful4082@gmail.com>
2021-07-11 00:57:15 +0600

    use fetch to load data from server, middleware , handle cours 47.6

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+const users = ['Asad', 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana']
+
+app.get('/', (req, res) => {
+    const fruit = {
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit)
+})
+
+app.get('/fruits/banana', (req, res)=>{
+    res.send({fruit:'Banan', quantiry: 1000, price: 10000})
+})
+app.get('/users/:id', (req, res)=>{
+    const id = req.params.id
+    const name = users[id]
+    res.send({id, name})
+    console.log(req.query.sort);
+})
+
+app.listen(4200, () => console.log('Listenting to port 4200'))
