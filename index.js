9c9949085c3a6120cffe1eba49cddd0bb52a4061
ariful4082 <ariful4082@gmail.com>
2021-07-10 23:37:23 +0600

    install nodemon , api return object, create multiple api

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('I Know How to open Node.... YAY')
+    const fruit = {
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit)
+})
+
+app.get('/fruits/banana', (req, res)=>{
+    res.send({fruit:'Banan', quantiry: 1000, price: 10000})
 })
 
 app.listen(4200, () => console.log('Listenting to port 4200'))
