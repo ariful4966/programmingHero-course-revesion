f5a08a2da721ec846962b98f582229a198ae219c
ariful4082 <ariful4082@gmail.com>
2021-07-11 22:31:59 +0600

    create post api install postman read post data, use body parser 47.7

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(cors()); //for get request
+app.use(bodyParser.json()) //for post request
 const users = ['Asad', 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana']
 
 app.get('/', (req, res) => {
@@ -13,14 +15,16 @@ app.get('/', (req, res) => {
     res.send(fruit)
 })
 
-app.get('/fruits/banana', (req, res)=>{
-    res.send({fruit:'Banan', quantiry: 1000, price: 10000})
-})
 app.get('/users/:id', (req, res)=>{
     const id = req.params.id
     const name = users[id]
     res.send({id, name})
-    console.log(req.query.sort);
+    console.log(req.params);
+})
+
+// post
+app.post('/addUser', (req, res)=>{
+    console.log(req.body);
 })
 
 app.listen(4200, () => console.log('Listenting to port 4200'))
