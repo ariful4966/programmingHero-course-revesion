61ee00a02d295e6651c421b9b4334ada53719be0
ariful4082 <ariful4082@gmail.com>
2021-07-12 22:57:17 +0600

    save data in database and Read all data 48.5

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res)=>{
-    res.send('Hello I am working');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
 })
 
 const uri = "mongodb+srv://nodeMongo:nodeMongo@cluster0.nine7.mongodb.net/organicdb?retryWrites=true&w=majority";
@@ -16,12 +17,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
   const collection = client.db("organicdb").collection("products");
-  console.log('Database connected'); 
-  client.close();
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    collection.insertOne(product)
+      .then(result => {
+        console.log('data added successfully');
+        res.send('success')
+      })
+  })
 });
 
 
 
-app.listen(4000, ()=>{
-    console.log('Running server is port 4000');
+app.listen(4000, () => {
+  console.log('Running server is port 4000');
 })
