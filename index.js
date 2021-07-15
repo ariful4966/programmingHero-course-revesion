f47f1431b4f7ae54d7c3284fa01043ea0a188f93
ariful4082 <ariful4082@gmail.com>
2021-07-15 07:34:09 +0600

    make CRUD operation working smoothly 48.10

--- index.js
+++ index.js
@@ -36,23 +36,22 @@ client.connect(err => {
     const product = req.body;
     collection.insertOne(product)
       .then(result => {
-        console.log('data added successfully');
-        res.send('success')
+        res.redirect('/')
       })
   })
   app.patch('/update/:id', (req, res) => {
     collection.updateOne({ _id: ObjectId(req.params.id) }, {
       $set: { price: req.body.price, quantity: req.body.quantity }
     })
-    .then(result=>{
-      console.log(result.modifiedCount);
-    })
+      .then(result => {
+        res.send(result.modifiedCount > 0)
+      })
   })
 
   app.delete('/delete/:id', (req, res) => {
     collection.deleteOne({ _id: ObjectId(req.params.id) })
       .then((result) => {
-        console.log(result.deletedCount);
+        res.send(result.deletedCount > 0)
       })
   })
 });
