3f745c0131868feb14cadc7ae3e8d774f3e5612e
ariful4082 <ariful4082@gmail.com>
2021-07-19 00:26:10 +0600

    insert bulk data many data to the databse 50.3

--- ema-john-server/index.js
+++ ema-john-server/index.js
@@ -1,37 +1,42 @@
-require('dotenv').config()
+require('dotenv').config(); 
 const express = require('express')
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const cors = require('cors')
+
+
 const app = express()
 
 
-app.use(bodyParser.json())
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.nine7.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nine7.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const products = client.db("emaJohnStore").collection("products");
+  const collection = client.db("emaJohnStore").collection("products");
   console.log('Ema-John-Store database connected successfully');
 
 
   app.get('/', (req, res) => {
     res.send('Hello ema watson!')
   });
-  
-  app.post('/addProduct', (req, res)=>{
-    const product = req.body()
-    products.insertOne(product)
-    .then(result=>{
-      console.log(result);
-    })
-  })
-  app.listen(process.env.PORT, () => {
-    console.log(`Server app listening at http://localhost:${process.env.PORT}`)
-  })
 
+  app.post('/addProduct', (req, res) => {
+    const products = req.body
+    
+    collection.insertMany(products)
+      .then(result => {
+        console.log(result.insertedCount);
+        res.send(result.insertedCount)
+      })
+  })
 
 
 })
+
+app.listen(process.env.PORT) 
 
