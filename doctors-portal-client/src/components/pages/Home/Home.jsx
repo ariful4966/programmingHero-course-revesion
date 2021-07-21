1519b8b037d02532debfd31a4b6b3789068b5b23
ariful4082 <ariful4082@gmail.com>
2021-07-22 02:02:46 +0600

    header part is runnig for the desin ui

---
+++ doctors-portal-client/src/components/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../partial/Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>This is Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ doctors-portal-client/src/components/pages/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <Typography variant="h1">
+                404
+            </Typography>
+            <Typography variant="paragraph">Sorry! Your </Typography>
+        </div>
+    );
+};
+
+export default NotFound;
