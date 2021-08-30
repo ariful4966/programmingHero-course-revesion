5520828411ea064ff05cc4c5b201d4fb946aee63
ariful4082 <ariful4082@gmail.com>
2021-08-30 23:21:19 +0600

    porotorypical inheritance and desing pattern 58.10

---
+++ prototype.js
@@ -0,0 +1,11 @@
+function Cat(name) {
+    this.name = name;
+}
+
+Cat.prototype.talk = function () {
+    console.log('meow meow');
+}
+
+const myCat = new Cat('Tiger')
+console.log(myCat);
+myCat.talk();
